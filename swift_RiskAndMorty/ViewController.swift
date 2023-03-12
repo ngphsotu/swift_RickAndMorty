85f502b570b20de0f40ef4d08edd901435b9165b
ngphsotu <ngphsotu@gmail.com>
2023-03-12 15:09:14 +0700

    Initial Commit

---
+++ swift_RiskAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_RiskAndMorty
+//
+//  Created by Tung on 12/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
